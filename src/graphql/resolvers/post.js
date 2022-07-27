6227c7f86c380819c1608aefef5c95b58a28b27b
ravinduf <ravinduf@rocketmail.com>
2022-07-27 20:25:55 +0530

    save tips and post done

--- src/graphql/resolvers/post.js
+++ src/graphql/resolvers/post.js
@@ -2,13 +2,19 @@ import { ErrorResponse } from "../../util/errorHandler/errorResponse.js";
 
 
 export default {
-    Mutation: {
-        createRecipe: async (_, args, { dataSources }) => {
-            console.log("resolver", args);
-            const { recipeInput } = args
-            const recipe = await dataSources.PostAPI.createRecipePost(recipeInput)
-            console.log("resolver", recipe)
-            return recipe;
-        }
+  Mutation: {
+    createRecipe: async (_, { recipeInput }, { dataSources }) => {
+      const recipe = await dataSources.PostAPI.createRecipe(recipeInput)
+      console.log("resolver", recipe)
+      return recipe;
+    },
+    createTips: async (_, { tipsInput }, { dataSources }) => {
+      const tips = await dataSources.PostAPI.createTips(tipsInput)
+      return tips;
+    },
+    createPost: async (_, { postInput }, { dataSources }) => {
+      const post = await dataSources.PostAPI.createPost(postInput)
+      return post;
     }
+  }
 }
--- src/graphql/schemas/post.js
+++ src/graphql/schemas/post.js
@@ -40,7 +40,6 @@ export default gql`
     }
 
     input TipsInput {
-        id: Int!
         title: String!
         tips: String!
         images: [String]
